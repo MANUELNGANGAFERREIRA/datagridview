cc60bfe1d1c28138eb8e22a066656926bcfc3068
ManueL Ferreira <160949381+MANUELNGANGAFERREIRA@users.noreply.github.com>
2024-10-17 19:59:24 +0100

    Add files via upload

---
+++ olamundo/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿namespace olamundo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(22, 55);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 51;
+            this.dataGridView1.RowTemplate.Height = 24;
+            this.dataGridView1.Size = new System.Drawing.Size(1472, 674);
+            this.dataGridView1.TabIndex = 0;
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(30, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(1084, 29);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "a area avermelhada é para o veneno de potência 2; a area amarelada é para o venen" +
+    "o de potência 1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(1239, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(210, 40);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "refrescar";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1471, 741);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.dataGridView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ olamundo/Form1.cs
@@ -0,0 +1,202 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace olamundo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+            DataTable table = new DataTable();
+            table.Columns.Add("Rua1",typeof(int));
+            table.Columns.Add("Rua2", typeof(int));
+            table.Columns.Add("Rua3", typeof(int));
+            table.Columns.Add("Rua4", typeof(int));
+            table.Columns.Add("Rua5", typeof(int));
+            table.Columns.Add("Rua6", typeof(int));
+            table.Columns.Add("Rua7", typeof(int));
+            table.Columns.Add("Rua8", typeof(int));
+            table.Columns.Add("Rua9", typeof(int));
+            table.Columns.Add("Rua10", typeof(int));
+            dataGridView1.DataSource = table;
+            Random random = new Random();
+            /*table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+            table.Rows.Add(random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15), random.Next(1, 15));
+           */
+                        table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+            table.Rows.Add(random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3), random.Next(1, 3));
+
+            /*agora começa a saga da potencia 2, aqui é que a brincadeira fica mais bonita  */
+            int valorquadrabig = 0;
+            int acumulantemaiorbig = 0;
+            int a1 = 0;
+            int a2 = 0;
+            int a3 = 0;
+            int a4 = 0;
+            int b1 = 0;
+            int b2 = 0;
+            int b3 = 0;
+            int b4 = 0;
+            int z1 = 0;
+            int w1 = 0;
+            for (int i = 0; i < 6; i++)
+            {
+                for (int j = 0; j < 6; j++)
+                {
+                    valorquadrabig += Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1 + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1 + 1 + 1].Value);
+
+                    valorquadrabig += Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1 ].Cells[j + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i +1].Cells[j + 1 + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i+1].Cells[j + 1 + 1 + 1].Value);
+                   
+                    valorquadrabig += Convert.ToInt32(dataGridView1.Rows[i + 1+1].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1+1].Cells[j + 1+1].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1+1].Cells[j + 1 + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1+1].Cells[j + 1 + 1 + 1].Value);
+                  
+                    valorquadrabig += Convert.ToInt32(dataGridView1.Rows[i + 1 + 1+1].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1 + 1+1].Cells[j + 1 + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1 + 1+1].Cells[j + 1 + 1].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1 + 1+1].Cells[j + 1 + 1 + 1].Value);
+
+                    if (acumulantemaiorbig < valorquadrabig)
+                    {
+                        acumulantemaiorbig = valorquadrabig;
+                        z1 = i;
+                        w1 = j;
+                        a1 = i + 1 + 1; 
+                        b1 = j + 1;
+                        a2 = i + 1;
+                        b2 = j + 1 + 1;
+                        a3 = i + 1 + 1;
+                        b3 = j + 1 + 1 + 1;
+                        a4 = i + 1 + 1 + 1;
+                        b4 = j + 1 + 1 + 1 + 1;
+                    }
+                    valorquadrabig = 0;
+
+
+
+
+                }
+
+
+            }
+
+
+
+            dataGridView1.Rows[z1].Cells[b1].Style.BackColor = Color.Red;
+            dataGridView1.Rows[z1].Cells[b2].Style.BackColor = Color.Red;
+            dataGridView1.Rows[z1].Cells[b3].Style.BackColor = Color.Red;
+            dataGridView1.Rows[z1].Cells[b4].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a1].Cells[b1].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a1].Cells[b2].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a1].Cells[b3].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a1].Cells[b4].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a2].Cells[b1].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a2].Cells[b2].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a2].Cells[b3].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a2].Cells[b4].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a3].Cells[b1].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a3].Cells[b2].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a3].Cells[b3].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a3].Cells[b4].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a4].Cells[b1].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a4].Cells[b2].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a4].Cells[b3].Style.BackColor = Color.Red;
+            dataGridView1.Rows[a4].Cells[b4].Style.BackColor = Color.Red;
+
+
+            /* essa é a parte do veneno potencia 1*/
+            int valorquadra = 0;
+            int acumulantemaior = 0;
+            int x1 = 0;
+            int x2 = 0;
+            int y1 = 0;
+            int y2 = 0;
+            for (int i = 0; i < 9; i++)
+            {
+                for (int j = 0; j < 9; j++)
+                {
+
+
+                    valorquadra += Convert.ToInt32(dataGridView1.Rows[i].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i].Cells[j + 1].Value);
+                    valorquadra += Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[j].Value) + Convert.ToInt32(dataGridView1.Rows[i + 1].Cells[j + 1].Value);
+
+
+                    if (dataGridView1.Rows[i].Cells[j].Style.BackColor == Color.Red ||
+           dataGridView1.Rows[i].Cells[j + 1].Style.BackColor == Color.Red ||
+           dataGridView1.Rows[i + 1].Cells[j].Style.BackColor == Color.Red ||
+           dataGridView1.Rows[i + 1].Cells[j + 1].Style.BackColor == Color.Red)
+                    {
+                        valorquadra = acumulantemaior;
+                    }
+                    if (acumulantemaior < valorquadra)
+                    {
+                        acumulantemaior = valorquadra;
+                        x1 = i; y1 = j; x2 = i + 1; y2 = j + 1;
+
+                    }
+                    valorquadra = 0;
+
+
+
+
+                }
+
+
+            }
+            /*as cores amarelas ficavam aqui vas o vermelho se subreposia muito entao lhe atirei la embaixo*/
+
+
+            /*termina aqui a potencia 1*/
+
+
+            /*agora taqui o amarelo*/
+            dataGridView1.Rows[x1].Cells[y1].Style.BackColor = Color.Yellow;
+            dataGridView1.Rows[x2].Cells[y1].Style.BackColor = Color.Yellow;
+            dataGridView1.Rows[x1].Cells[y2].Style.BackColor = Color.Yellow;
+            dataGridView1.Rows[x2].Cells[y2].Style.BackColor = Color.Yellow;
+
+            /*termina aqui a grande saga felismente teriminei*/
+
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Application.Restart();
+        }
+    }
+}
